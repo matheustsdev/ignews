95b7bc221684230f35ff3a3fad0a5b8a4f2a931f
Matheus Teixeira da Silva <matheusts.dev@gmail.com>
2022-01-25 17:32:36 -0300

    Auth function

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,18 +1,29 @@
 import styles from "./styles.module.scss";
+import { signIn, signOut, useSession } from "next-auth/react";
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const { data: session } = useSession();
 
-  return isUserLoggedIn ? (
-    <button type="button" className={styles.signInButton}>
+  console.log(session);
+
+  return session ? (
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signOut()}
+    >
       <FaGithub color="#04d361" />
-      Matheus Teixeira
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button type="button" className={styles.signInButton}>
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signIn("github")}
+    >
       <FaGithub color="#eba417" />
       Sign in with Github
     </button>
---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,35 @@
+import { query as q } from "faunadb";
+
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+
+import { fauna } from "../../../services/fauna";
+
+export default NextAuth({
+  // Configure one or more authentication providers
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      authorization: {
+        params: {
+          scope: "read:user",
+        },
+      },
+    }),
+  ],
+
+  callbacks: {
+    async signIn({ user, account, profile, credentials }) {
+      const { email } = user;
+
+      try {
+        await fauna.query(q.Create(q.Collection("users"), { data: { email } }));
+      } catch {
+        return false;
+      }
+
+      return true;
+    },
+  },
+});
