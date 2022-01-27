914013638f6dcae0e65b2fdf229f724a72afd0cb
Matheus Teixeira da Silva <matheusts.dev@gmail.com>
2022-01-27 20:31:22 -0300

    Stripe running and infos in db

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -2,6 +2,7 @@ import { query as q } from "faunadb";
 
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import { env } from "process";
 
 import { fauna } from "../../../services/fauna";
 
@@ -24,7 +25,17 @@ export default NextAuth({
       const { email } = user;
 
       try {
-        await fauna.query(q.Create(q.Collection("users"), { data: { email } }));
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(q.Index("user_by_email"), q.Casefold(user.email))
+              )
+            ),
+            q.Create(q.Collection("users"), { data: { email } }),
+            q.Get(q.Match(q.Index("user_by_email"), q.Casefold(user.email)))
+          )
+        );
       } catch {
         return false;
       }
