6d545196596f8a2fd65d597f779c353d0ccde9b9
Matheus Teixeira da Silva <matheusts.dev@gmail.com>
2022-01-28 17:59:23 -0300

    Start connect with prismic.io

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -34,7 +34,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       await fauna.query(
         q.Update(q.Ref(q.Collection("users"), user.ref.id), {
           data: {
-            stripe_costume_id: stripeCostumer.id,
+            stripe_customer_id: stripeCostumer.id,
           },
         })
       );
